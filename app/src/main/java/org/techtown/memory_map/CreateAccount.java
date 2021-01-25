1da39d6dd99a845be30d876fc9b29ba5de3982f9
dayeondayeon <dayeon2438@gmail.com>
2021-01-26 00:30:44 +0900

    Login View & Create Account View change

---
+++ app/src/main/java/org/techtown/memory_map/CreateAccount.java
@@ -0,0 +1,26 @@
+package org.techtown.memory_map;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccount extends AppCompatActivity {
+    Button button;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+        button=findViewById(R.id.createButton);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/org/techtown/memory_map/MainActivity.java
+++ app/src/main/java/org/techtown/memory_map/MainActivity.java
@@ -2,13 +2,34 @@ package org.techtown.memory_map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText idInput;
+    EditText pwInput;
+    public static final int REQUEST_CODE_CREATE_ACCOUNT = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_account);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.no_account_yet);
+        idInput=findViewById(R.id.input_id_Main);
+        pwInput=findViewById(R.id.input_pw_Main);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+                startActivityForResult(intent, REQUEST_CODE_CREATE_ACCOUNT);
+            }
+        });
     }
+
 }
