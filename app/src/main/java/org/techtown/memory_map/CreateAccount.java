bb030a578bc07ab4c5b9e043fd363d09160d6ec3
dayeondayeon <dayeon2438@gmail.com>
2021-01-26 02:13:39 +0900

    Set Toast Message at Login/CreateAccount

--- app/src/main/java/org/techtown/memory_map/CreateAccount.java
+++ app/src/main/java/org/techtown/memory_map/CreateAccount.java
@@ -4,22 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccount extends AppCompatActivity {
     Button button;
+    EditText id_input;
+    EditText pw_input;
+    EditText username_input;
+    EditText email_input;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
 
         button=findViewById(R.id.createButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        id_input = findViewById(R.id.input_id);
+        pw_input = findViewById(R.id.input_pw);
+        username_input = findViewById(R.id.input_username);
+        email_input = findViewById(R.id.input_email);
 
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent resultIntent = new Intent();
-                finish();
+                if((id_input.length() == 0) ||(pw_input.length() == 0) || (username_input.length() == 0) || (email_input.length() == 0)){
+                    Toast.makeText(getApplicationContext(), "please enter all information",Toast.LENGTH_SHORT).show();
+                } else {
+                    String id = id_input.getText().toString();
+                    String pw = pw_input.getText().toString();
+                    String username = username_input.getText().toString();
+                    String email = email_input.getText().toString();
+                    Intent resultIntent = new Intent();
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/org/techtown/memory_map/MainActivity.java
+++ app/src/main/java/org/techtown/memory_map/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText idInput;
     EditText pwInput;
+    Button sign_in_button;
     public static final int REQUEST_CODE_CREATE_ACCOUNT = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         textView=findViewById(R.id.no_account_yet);
         idInput=findViewById(R.id.input_id_Main);
         pwInput=findViewById(R.id.input_pw_Main);
-
+        sign_in_button = findViewById(R.id.signInBtn_Main);
         textView.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -30,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_CREATE_ACCOUNT);
             }
         });
+
+        sign_in_button.setOnClickListener((new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if((idInput.length() == 0) || (pwInput.length() == 0)){
+                    Toast.makeText(getApplicationContext(), "must enter all information",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }));
     }
 
 }
