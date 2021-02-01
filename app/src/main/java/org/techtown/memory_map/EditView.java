e22bec3fda27120fbbc1b83e97c9fa94499cf846
Happiva <kiwhhb1016@gmail.com>
2021-02-01 14:33:46 +0900

    EditView 사진 첨부 기능 구현 중

--- app/src/main/java/org/techtown/memory_map/EditView.java
+++ app/src/main/java/org/techtown/memory_map/EditView.java
@@ -1,14 +1,142 @@
 package org.techtown.memory_map;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class EditView extends Fragment {
+
+    private ImageView edit_image;
+    private Context context;
+    private static final int GALLERY_REQUEST_CODE = 301;
+
+    private File tempFile;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_editview, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_editview, container, false);
+
+        context = container.getContext();
+
+        initUI(rootView);
+
+        tedPermission();
+
+        return rootView;
+    }
+
+    private void initUI(ViewGroup rootView) {
+        edit_image = (ImageView)rootView.findViewById(R.id.edit_Image);
+
+        edit_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GALLERY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(context, "취소 되었습니다.", Toast.LENGTH_LONG).show();
+
+            if (tempFile != null) {
+                if (tempFile.exists()) {
+                    if (tempFile.delete()) {
+                        tempFile = null;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            Uri photoUri = data.getData();
+            Cursor cursor = null;
+
+            try {
+                String[] proj = {MediaStore.Images.Media.DATA};
+
+                assert photoUri != null;
+                cursor = getContext().getContentResolver().query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                cursor.moveToFirst();
+
+                tempFile = new File(cursor.getString(column_index));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+        }
+    }
+
+    private void setImage() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+
+        edit_image.setImageBitmap(originalBm);
+    }
+
+    private void tedPermission() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+
+            }
+        };
+
+        TedPermission.with(context)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("사진 첨부를 위해서 접근 권한이 필요합니다.")
+                .setDeniedMessage("[설정] > [권한] 에서 권한을 허용할 수 있습니다.")
+                .setPermissions(new String [] {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                })
+                .check();
+
+
     }
 }
