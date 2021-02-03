8ad359fac8e8a4e0902986d19c7a933cb2808f01
dayeondayeon <dayeon2438@gmail.com>
2021-02-03 23:44:56 +0900

    SettingView fin

--- app/src/main/java/org/techtown/memory_map/SettingView.java
+++ app/src/main/java/org/techtown/memory_map/SettingView.java
@@ -2,13 +2,46 @@ package org.techtown.memory_map;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
 public class SettingView extends Fragment {
+    Spinner spinner;
+    TextView choice;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settingview, container, false);
+        View root = inflater.inflate(R.layout.fragment_settingview, container, false);
+        spinner = root.findViewById(R.id.spinner_language);
+        choice = root.findViewById(R.id.spinner_choice);
+        final String[] fields = {"한국어", "English"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item, fields);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                choice.setText(fields[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return root;
     }
 }
